55df5beafb17f49a37ddbc890e33f4309068fbaf
Mistsuu <vietanhtran12@gmail.com>
2023-01-23 17:31:21 +0700

    Update file structure

--- miscellaneous/glob.c
+++
@@ -1,9 +0,0 @@
-#include "gmp.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
-
-int main(int argc, char * argv[]) {
-    printf("%s\n", gmp_version);
-    printf("%d\n", mp_bits_per_limb);
-}
