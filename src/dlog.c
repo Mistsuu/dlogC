5dd0d7f6f624ff93c114600539638e3a8e89c22a
Mistsuu <vietanhtran12@gmail.com>
2023-01-25 14:31:37 +0700

    Refactor arguments of dlog.c to fit multithread model

--- src/dlog.c
+++ src/dlog.c
@@ -25,11 +25,11 @@ size_t dlog_init_buffer(
         n_size_t ^=  nitems_alloc_limbs[i];
     }
 
-    // Checks if n_size_t items fits in size_t
+    // Checks if size of n_size_t items fits in size_t
     if (SIZE_MAX / (index_size_bytes + item_size_bytes) / 2 < n_size_t)
         return 0;
 
-    // Now we allocate (and set 0 the whole area :>)
+    // Now we allocate
     size_t nbytes_alloc = (n_size_t + 1) * (index_size_bytes + item_size_bytes);
     printf("[debug] size buffer: %ld bytes = %f MB = %f GB\n", 
                 nbytes_alloc * 2, 
@@ -42,98 +42,81 @@ size_t dlog_init_buffer(
 }
 
 void __thread__dlog_fill_buffer(
-    char* lbuffer,
-    char* rbuffer,
-
-    size_t n_size_t, 
-    size_t index_size_bytes, size_t index_size_limbs,
-    size_t item_size_bytes, size_t item_size_limbs,
-
-    mp_limb_t* L0x, mp_limb_t* L0z,   // must be modified in-use
-    mp_limb_t* L1x, mp_limb_t* L1z,   // must be modified in-use
-    mp_limb_t* R0x, mp_limb_t* R0z,   // must be modified in-use
-    mp_limb_t* R1x, mp_limb_t* R1z,   // must be modified in-use
-    mp_limb_t* Px, mp_limb_t* Pz,
-
-    mp_limb_t* i_item_l, mp_limb_t* i_item_r,
-
-    mp_limb_t* curve_a,
-    mp_limb_t* curve_b,
-    mp_limb_t* curve_p
+    __args_thread__dlog_fill_buffer* args
 )
 {   
     ecc_xtemp T;
-    ecc_init_xtemp(T, item_size_limbs);
+    ecc_init_xtemp(T, args->item_size_limbs);
 
-    mp_limb_t* V0 = (mp_limb_t*) malloc(sizeof(mp_limb_t) * item_size_limbs);
-    mp_limb_t* V1 = (mp_limb_t*) malloc(sizeof(mp_limb_t) * item_size_limbs);
+    mp_limb_t* V0 = (mp_limb_t*) malloc(sizeof(mp_limb_t) * args->item_size_limbs);
+    mp_limb_t* V1 = (mp_limb_t*) malloc(sizeof(mp_limb_t) * args->item_size_limbs);
 
-    for (size_t _ = 0; _ < n_size_t; ++_) {
+    for (size_t _ = 0; _ < args->n_size_t; ++_) {
         // ---------------------- Write iL -----------------------
-        mpn2bytes(lbuffer, index_size_bytes, i_item_l, index_size_limbs);
-        lbuffer += index_size_bytes;
+        mpn2bytes(args->lbuffer, args->index_size_bytes, args->i_l, args->index_size_limbs);
+        args->lbuffer += args->index_size_bytes;
 
         // ---------------------- Write L0x/L0z -----------------------
-        if (ecc_xz_to_X(V0, L0x, L0z, curve_p, item_size_limbs, T))
-            mpn2bytes(lbuffer, item_size_bytes, V0, item_size_limbs);
+        if (ecc_xz_to_X(V0, args->L0x, args->L0z, args->curve_p, args->item_size_limbs, T))
+            mpn2bytes(args->lbuffer, args->item_size_bytes, V0, args->item_size_limbs);
         else 
-            mpn2bytes(lbuffer, item_size_bytes, curve_p, item_size_limbs);
-        lbuffer += item_size_bytes;
+            mpn2bytes(args->lbuffer, args->item_size_bytes, args->curve_p, args->item_size_limbs);
+        args->lbuffer += args->item_size_bytes;
 
         // ---------------------- Write iR -----------------------
-        mpn2bytes(rbuffer, index_size_bytes, i_item_r, index_size_limbs);
-        rbuffer += index_size_bytes;
+        mpn2bytes(args->rbuffer, args->index_size_bytes, args->i_r, args->index_size_limbs);
+        args->rbuffer += args->index_size_bytes;
 
         // ---------------------- Write R0x/R0z -----------------------
-        if (ecc_xz_to_X(V0, R0x, R0z, curve_p, item_size_limbs, T))
-            mpn2bytes(rbuffer, item_size_bytes, V0, item_size_limbs);
+        if (ecc_xz_to_X(V0, args->R0x, args->R0z, args->curve_p, args->item_size_limbs, T))
+            mpn2bytes(args->rbuffer, args->item_size_bytes, V0, args->item_size_limbs);
         else 
-            mpn2bytes(rbuffer, item_size_bytes, curve_p, item_size_limbs);
-        rbuffer += item_size_bytes;
+            mpn2bytes(args->rbuffer, args->item_size_bytes, args->curve_p, args->item_size_limbs);
+        args->rbuffer += args->item_size_bytes;
 
         // ---------------------- Update iL -----------------------
-        mpn_add_1(i_item_l, i_item_l, index_size_limbs, 1);
+        mpn_add_1(args->i_l, args->i_l, args->index_size_limbs, 1);
 
         // ---------------------- Update iR -----------------------
-        mpn_sub_1(i_item_r, i_item_r, index_size_limbs, 1);
+        mpn_sub_1(args->i_r, args->i_r, args->index_size_limbs, 1);
 
         // ---------------------- Update L*x,L*z -----------------------
         ecc_xadd(
             V0, V1,
-            L1x, L1z,
-            Px, Pz,
-            L0x, L0z,
+            args->L1x, args->L1z,
+            args->Gx, args->Gz,
+            args->L0x, args->L0z,
 
-            curve_a,
-            curve_b,
-            curve_p,
-            item_size_limbs,
+            args->curve_a,
+            args->curve_b,
+            args->curve_p,
+            args->item_size_limbs,
 
             T
         );
-        mpn_copyd(L0x, L1x, item_size_limbs);
-        mpn_copyd(L0z, L1z, item_size_limbs);
-        mpn_copyd(L1x, V0, item_size_limbs);
-        mpn_copyd(L1z, V1, item_size_limbs);
+        mpn_copyd(args->L0x, args->L1x, args->item_size_limbs);
+        mpn_copyd(args->L0z, args->L1z, args->item_size_limbs);
+        mpn_copyd(args->L1x, V0, args->item_size_limbs);
+        mpn_copyd(args->L1z, V1, args->item_size_limbs);
 
         // ---------------------- Update R*x,R*z -----------------------
         ecc_xadd(
             V0, V1,
-            R1x, R1z,
-            Px, Pz,
-            R0x, R0z,
+            args->R1x, args->R1z,
+            args->nGx, args->nGz,
+            args->R0x, args->R0z,
 
-            curve_a,
-            curve_b,
-            curve_p,
-            item_size_limbs,
+            args->curve_a,
+            args->curve_b,
+            args->curve_p,
+            args->item_size_limbs,
 
             T
         );
-        mpn_copyd(R0x, R1x, item_size_limbs);
-        mpn_copyd(R0z, R1z, item_size_limbs);
-        mpn_copyd(R1x, V0, item_size_limbs);
-        mpn_copyd(R1z, V1, item_size_limbs);
+        mpn_copyd(args->R0x, args->R1x, args->item_size_limbs);
+        mpn_copyd(args->R0z, args->R1z, args->item_size_limbs);
+        mpn_copyd(args->R1x, V0, args->item_size_limbs);
+        mpn_copyd(args->R1z, V1, args->item_size_limbs);
     }
 
     ecc_free_xtemp(T);
@@ -153,81 +136,179 @@ void dlog_fill_buffer(
     unsigned int n_threads
 )
 {
+    // -------------------------------------------------------------------------------------
+    //      Set up number of items per thread.
+    // -------------------------------------------------------------------------------------
     mpz_t n_per_thread;
-    mpz_init_set(n_per_thread, n);
-
     mpz_t n_last_thread;
+    mpz_init_set(n_per_thread, n);
     mpz_init(n_last_thread);
-
     mpz_tdiv_qr_ui(n_per_thread, n_last_thread, n_per_thread, (unsigned long)n_threads);
     mpz_add(n_last_thread, n_last_thread, n_per_thread);
 
-    eccpt L0; ecc_init_pt_pt(L0, G);    // L0 <- G
-    eccpt L1; ecc_init_pt_pt(L1, G);    
-    ecc_add(curve, L1, L1, L1);         // L1 <- 2*G
+    // -------------------------------------------------------------------------------------
+    //      Write [ 0 |     0*G    ] to lbuffer, 
+    //            [ n | (k - n*n)G ] to rbuffer.
+    // -------------------------------------------------------------------------------------
+    eccpt kG_nnG; ecc_init_pt_pt(kG_nnG, G);    // kG - nnG
+    ecc_mul(curve, kG_nnG, kG_nnG, n);
+    ecc_mul(curve, kG_nnG, kG_nnG, n);
+    ecc_sub(curve, kG_nnG, kG, kG_nnG);
 
-    eccpt _nnG; ecc_init_pt_pt(_nnG, G);
-    ecc_mul(curve, _nnG, _nnG, n);
-    ecc_mul(curve, _nnG, _nnG, n);
-    mpz_sub(_nnG->y, curve->p, _nnG->y);
+    // [ 0 |     0*G    ] -> lbuffer
+    memset(lbuffer, 0, index_size_bytes); 
+    lbuffer += index_size_bytes;
+    mpn2bytes(lbuffer, item_size_bytes, mpz_limbs_read(curve->p), item_size_limbs);
+    lbuffer += item_size_bytes;
 
-    eccpt R0; ecc_init_pt_pt(R0, kG);   // R0 <- (k-n*n)*G
-    ecc_add(curve, R0, R0, _nnG);
+    // [ n | (k - n*n)G ] -> rbuffer
+    mpn2bytes(rbuffer, index_size_bytes, mpz_limbs_read(n), index_size_limbs);
+    rbuffer += index_size_bytes;
+    mpn2bytes(rbuffer, item_size_bytes, mpz_limbs_read(kG_nnG->x), item_size_limbs);
+    rbuffer += item_size_bytes;
+
+    // -------------------------------------------------------------------------------------
+    //      Set up L0 & L1.
+    // -------------------------------------------------------------------------------------
+    eccpt L0; ecc_init_pt_pt(L0, G);            // L0 <- 1*G
+    eccpt L1; ecc_init_pt(L1);                  // L1 <- 2*G
+    ecc_add(curve, L1, G, G);                 
+
+    eccpt nG; ecc_init_pt_pt(nG, G);
+    ecc_mul(curve, nG, nG, n);
+    eccpt R0; ecc_init_pt_pt(R0, kG_nnG);       // R0 <- (k - n*n + 1*n)*G
+    ecc_add(curve, R0, R0, nG);
     eccpt R1; ecc_init_pt_pt(R1, R0);    
-    ecc_add(curve, R1, R1, G);          // R1 <- (k-n*n+1)*G
+    ecc_add(curve, R1, R1, nG);                 // R1 <- (k - n*n + 2*n)*G
 
-    mp_limb_t* i_item_l = mpz_limbs_init_zero(index_size_limbs);
-    mp_limb_t* i_item_r = mpz_limbs_init_cpy(n_per_thread, index_size_limbs);
+    // -------------------------------------------------------------------------------------
+    //      Initialize memory for arguments of __thread__dlog_fill_buffer.
+    // -------------------------------------------------------------------------------------
+
+    __args_thread__dlog_fill_buffer* thread_args = (__args_thread__dlog_fill_buffer*) malloc(sizeof(__args_thread__dlog_fill_buffer) * n_threads);
+
     mp_limb_t* Gx = mpz_limbs_init_cpy(G->x, item_size_limbs);
     mp_limb_t* Gz = mpz_limbs_init_cpy(G->z, item_size_limbs);
-    mp_limb_t* L0x = mpz_limbs_init_cpy(L0->x, item_size_limbs);
-    mp_limb_t* L0z = mpz_limbs_init_cpy(L0->z, item_size_limbs);
-    mp_limb_t* L1x = mpz_limbs_init_cpy(L1->x, item_size_limbs);
-    mp_limb_t* L1z = mpz_limbs_init_cpy(L1->z, item_size_limbs);
-    mp_limb_t* R0x = mpz_limbs_init_cpy(R0->x, item_size_limbs);
-    mp_limb_t* R0z = mpz_limbs_init_cpy(R0->z, item_size_limbs);
-    mp_limb_t* R1x = mpz_limbs_init_cpy(R1->x, item_size_limbs);
-    mp_limb_t* R1z = mpz_limbs_init_cpy(R1->z, item_size_limbs);
+    mp_limb_t* nGx = mpz_limbs_init_cpy(nG->x, item_size_limbs);
+    mp_limb_t* nGz = mpz_limbs_init_cpy(nG->z, item_size_limbs);
     mp_limb_t* curve_a = mpz_limbs_init_cpy(curve->a, item_size_limbs);
     mp_limb_t* curve_b = mpz_limbs_init_cpy(curve->b, item_size_limbs);
     mp_limb_t* curve_p = mpz_limbs_init_cpy(curve->p, item_size_limbs);
 
+    mp_limb_t** i_l = (mp_limb_t**) malloc(sizeof(mp_limb_t*) * n_threads);
+    mp_limb_t** i_r = (mp_limb_t**) malloc(sizeof(mp_limb_t*) * n_threads);
+    mp_limb_t** L0x = (mp_limb_t**) malloc(sizeof(mp_limb_t*) * n_threads);
+    mp_limb_t** L0z = (mp_limb_t**) malloc(sizeof(mp_limb_t*) * n_threads);
+    mp_limb_t** L1x = (mp_limb_t**) malloc(sizeof(mp_limb_t*) * n_threads);
+    mp_limb_t** L1z = (mp_limb_t**) malloc(sizeof(mp_limb_t*) * n_threads);
+    mp_limb_t** R0x = (mp_limb_t**) malloc(sizeof(mp_limb_t*) * n_threads);
+    mp_limb_t** R0z = (mp_limb_t**) malloc(sizeof(mp_limb_t*) * n_threads);
+    mp_limb_t** R1x = (mp_limb_t**) malloc(sizeof(mp_limb_t*) * n_threads);
+    mp_limb_t** R1z = (mp_limb_t**) malloc(sizeof(mp_limb_t*) * n_threads);
+
+    eccpt n_per_threadG;
+    eccpt nn_per_threadG;
+    ecc_init_pt(n_per_threadG);
+    ecc_init_pt(nn_per_threadG);
+    ecc_mul(curve, n_per_threadG, G, n_per_thread);
+    ecc_mul(curve, nn_per_threadG, n_per_threadG, n);
+
+    mpz_t iL;
+    mpz_t iR;
+    mpz_init_set_si(iL, 1);     // Because we have written 0 to buffer, so index of lbuffer starts with 1.
+    mpz_init_set(iR, n);
+    mpz_sub_ui(iR, iR, 1);      // Because we have written n to buffer, so index of rbuffer starts with n-1.
+
+    for (unsigned int i = 0; i < n_threads; ++i) {
+        i_l[i] = mpz_limbs_init_cpy(iL, index_size_limbs);
+        i_r[i] = mpz_limbs_init_cpy(iR, index_size_limbs);
+        L0x[i] = mpz_limbs_init_cpy(L0->x, item_size_limbs);
+        L0z[i] = mpz_limbs_init_cpy(L0->z, item_size_limbs);
+        L1x[i] = mpz_limbs_init_cpy(L1->x, item_size_limbs);
+        L1z[i] = mpz_limbs_init_cpy(L1->z, item_size_limbs);
+        R0x[i] = mpz_limbs_init_cpy(R0->x, item_size_limbs);
+        R0z[i] = mpz_limbs_init_cpy(R0->z, item_size_limbs);
+        R1x[i] = mpz_limbs_init_cpy(R1->x, item_size_limbs);
+        R1z[i] = mpz_limbs_init_cpy(R1->z, item_size_limbs);
+
+        ecc_add(curve, L0, L0, n_per_threadG);      // L0 <- L0 + n_per_thread*G
+        ecc_add(curve, L1, L1, n_per_threadG);      // L1 <- L1 + n_per_thread*G
+        ecc_add(curve, R0, R0, nn_per_threadG);     // R0 <- R0 + n*n_per_thread*G
+        ecc_add(curve, R1, R1, nn_per_threadG);     // R1 <- R1 + n*n_per_thread*G
+        mpz_add(iL, iL, n_per_thread);
+        mpz_sub(iR, iR, n_per_thread);
+    }
+
+    // -------------------------------------------------------------------------------------
+    //      Generate threads.
+    // -------------------------------------------------------------------------------------
+
     size_t n_per_thread_size_t = n_size_t / n_threads;
     size_t n_last_thread_size_t = n_per_thread_size_t + n_size_t % n_threads;
-    __thread__dlog_fill_buffer(
-        lbuffer,
-        rbuffer,
+    // __thread__dlog_fill_buffer(
+    //     lbuffer,
+    //     rbuffer,
 
-        n_per_thread_size_t, 
-        index_size_bytes, index_size_limbs,
-        item_size_bytes, item_size_limbs,
+    //     n_per_thread_size_t, 
+    //     index_size_bytes, index_size_limbs,
+    //     item_size_bytes, item_size_limbs,
 
-        L0x, L0z,
-        L1x, L1z,
-        R0x, R0z,
-        R1x, R1z,
-        Gx, Gz,
+    //     L0x, L0z,
+    //     L1x, L1z,
+    //     R0x, R0z,
+    //     R1x, R1z,
+    //     Gx, Gz,
+    //     nGx, nGz,
 
-        i_item_l, i_item_r,
+    //     i_l, i_r,
 
-        curve_a,
-        curve_b,
-        curve_p
-    );
+    //     curve_a,
+    //     curve_b,
+    //     curve_p
+    // );
+
+
+    // -------------------------------------------------------------------------------------
+    //      Cleaning up.
+    // -------------------------------------------------------------------------------------
 
     mpz_clear(n_per_thread);
     mpz_clear(n_last_thread);
+    mpz_clear(iL);
+    mpz_clear(iR);
 
+    ecc_free_pt(nG);
+    ecc_free_pt(kG_nnG);
     ecc_free_pt(L0);
     ecc_free_pt(L1);
     ecc_free_pt(R0);
     ecc_free_pt(R1);
-    ecc_free_pt(_nnG);
+    ecc_free_pt(n_per_threadG);
+    ecc_free_pt(nn_per_threadG);
 
-    free(i_item_l);
-    free(i_item_r);
     free(Gx);
     free(Gz);
+    free(nGx);
+    free(nGz);
+    free(curve_a);
+    free(curve_b);
+    free(curve_p);
+
+    for (unsigned int i = 0; i < n_threads; ++i) {
+        free(i_l[i]);
+        free(i_r[i]);
+        free(L0x[i]);
+        free(L0z[i]);
+        free(L1x[i]);
+        free(L1z[i]);
+        free(R0x[i]);
+        free(R0z[i]);
+        free(R1x[i]);
+        free(R1z[i]);
+    }
+
+    free(i_l);
+    free(i_r);
     free(L0x);
     free(L0z);
     free(L1x);
@@ -236,9 +317,6 @@ void dlog_fill_buffer(
     free(R0z);
     free(R1x);
     free(R1z);
-    free(curve_a);
-    free(curve_b);
-    free(curve_p);
 }
 
 /*
@@ -282,6 +360,7 @@ int dlog(ecc curve, mpz_t k, eccpt G, eccpt kG, mpz_t upper_k, unsigned int n_th
     dlog_fill_buffer(
         lbuffer, 
         rbuffer, 
+
         curve, G, kG, 
         
         n, n_size_t, 
@@ -290,17 +369,29 @@ int dlog(ecc curve, mpz_t k, eccpt G, eccpt kG, mpz_t upper_k, unsigned int n_th
         
         n_threads
     );
+
     // dlog_sort_buffer(
-    //     buffer, 
-    //     n_size_t, index_size_bytes, item_size_bytes,
+    //     lbuffer,
+    //     rbuffer,
+    //
+    //     n_size_t, 
+    //     index_size_bytes, 
+    //     item_size_bytes,
+    //
     //     n_threads
     // );
 
     // mpz_t iL, iR;
     // dlog_search_buffer(
     //     iL, iR,
-    //     buffer, 
-    //     n_size_t, index_size_bytes, item_size_bytes,
+    //
+    //     lbuffer,
+    //     rbuffer,
+    //
+    //     n_size_t, 
+    //     index_size_bytes, 
+    //     item_size_bytes,
+    //
     //     n_threads
     // );
 
--- src/dlog.h
+++ src/dlog.h
@@ -16,27 +16,31 @@ size_t dlog_init_buffer(
 
 int dlog(ecc curve, mpz_t k, eccpt G, eccpt kG, mpz_t upper_k, unsigned int n_threads);
 
+typedef struct {
+    char* lbuffer;  
+    char* rbuffer;
+
+    size_t n_size_t; 
+    size_t index_size_bytes; size_t index_size_limbs;
+    size_t item_size_bytes; size_t item_size_limbs;
+
+    mp_limb_t* L0x; mp_limb_t* L0z;   // must be modified in-use
+    mp_limb_t* L1x; mp_limb_t* L1z;   // must be modified in-use
+    mp_limb_t* R0x; mp_limb_t* R0z;   // must be modified in-use
+    mp_limb_t* R1x; mp_limb_t* R1z;   // must be modified in-use
+    mp_limb_t* Gx; mp_limb_t* Gz;
+    mp_limb_t* nGx; mp_limb_t* nGz;
+
+    mp_limb_t* i_l; mp_limb_t* i_r;
+
+    mp_limb_t* curve_a;
+    mp_limb_t* curve_b;
+    mp_limb_t* curve_p;
+} __args_thread__dlog_fill_buffer;
+
 void __thread__dlog_fill_buffer(
-    char* lbuffer,
-    char* rbuffer,
-
-    size_t n_size_t, 
-    size_t index_size_bytes, size_t index_size_limbs,
-    size_t item_size_bytes, size_t item_size_limbs,
-
-    mp_limb_t* L0x, mp_limb_t* L0z,   // must be modified in-use
-    mp_limb_t* L1x, mp_limb_t* L1z,   // must be modified in-use
-    mp_limb_t* R0x, mp_limb_t* R0z,   // must be modified in-use
-    mp_limb_t* R1x, mp_limb_t* R1z,   // must be modified in-use
-    mp_limb_t* Px, mp_limb_t* Pz,
-
-    mp_limb_t* i_item_l, mp_limb_t* i_item_r,
-
-    mp_limb_t* curve_a,
-    mp_limb_t* curve_b,
-    mp_limb_t* curve_p
+    __args_thread__dlog_fill_buffer* args
 );
-
 
 void dlog_fill_buffer(
     char* lbuffer,
