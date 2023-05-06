2f2ac39033fa1039accf6117599082dea3749c9a
Mistsuu <vietanhtran12@gmail.com>
2023-05-06 21:21:17 +0700

    Let the user exit

--- src/sdlog.c
+++ src/sdlog.c
@@ -9,7 +9,7 @@ void user_interrupt_handler(
     int signum
 )
 {
-    printf("[libbsgsfp] Caught SIGINT! Exiting in peace...\n");
+    printf("[libbsgsefp] Caught SIGINT! Exiting in peace...\n");
     exit(-1);
 }
 
