1e719bcc7106c157bf0a52115bd4ad07f03c46c0
Mistsuu <vietanhtran12@gmail.com>
2023-06-04 18:52:07 +0700

    Just add and dbl only.

--- src/ecc_proj.c
+++ src/ecc_proj.c
@@ -10,7 +10,12 @@ void ecc_init_ptemp(ecc_ptemp T, mp_size_t n)
     T[3]  = (mp_limb_t*) malloc_exit_when_null(sizeof(mp_limb_t) * n);
     T[4]  = (mp_limb_t*) malloc_exit_when_null(sizeof(mp_limb_t) * n);
     T[5]  = (mp_limb_t*) malloc_exit_when_null(sizeof(mp_limb_t) * n);
-    T[6]  = (mp_limb_t*) malloc_exit_when_null(sizeof(mp_limb_t) * (6*n));
+    T[6]  = (mp_limb_t*) malloc_exit_when_null(sizeof(mp_limb_t) * n);
+    T[7]  = (mp_limb_t*) malloc_exit_when_null(sizeof(mp_limb_t) * n);
+    T[8]  = (mp_limb_t*) malloc_exit_when_null(sizeof(mp_limb_t) * n);
+    T[9]  = (mp_limb_t*) malloc_exit_when_null(sizeof(mp_limb_t) * n);
+    T[10] = (mp_limb_t*) malloc_exit_when_null(sizeof(mp_limb_t) * n);
+    T[11] = (mp_limb_t*) malloc_exit_when_null(sizeof(mp_limb_t) * (6*n));
 }
 
 void ecc_free_ptemp(ecc_ptemp T)
@@ -26,13 +31,11 @@ void ecc_free_ptemp(ecc_ptemp T)
 
 // -------------------------------------------------------------------------------
 
-void ecc_padd(
+void ecc_padd_Qz_is_1(
     mp_limb_t* Rx, mp_limb_t* Ry, mp_limb_t* Rz,       // Rx, Ry, Rz must have n limbs allocated
     mp_limb_t* Px, mp_limb_t* Py, mp_limb_t* Pz,       // Px, Ry, Pz must have n limbs allocated
-    mp_limb_t* Qx, mp_limb_t* Qy, mp_limb_t* Qz,       // Qx, Qy, Qz must have n limbs allocated
+    mp_limb_t* Qx, mp_limb_t* Qy,                      // Qx, Qy     must have n limbs allocated
 
-    mp_limb_t* curve_aR,                               // curve_aR must have n limbs allocated
-    mp_limb_t* curve_b3R,                              // curve_b3R must have n limbs allocated
     mp_limb_t* curve_p,                                // curve_p must have n limbs allocated
     mp_limb_t* curve_P,                                // curve_P must have n limbs allocated
     mp_size_t n,                                       // number of limbs in curve->p
@@ -40,86 +43,42 @@ void ecc_padd(
     ecc_ptemp T                                        // temporary variables, allocated with ecc_init_ptemp(T, n).
 )
 {
-    // t0 = X1*X2
-    mpn_montgomery_mulmod_n(T[0], Px, Qx, curve_p, curve_P, n, T[6]);
-    // t1 = Y1*Y2
-    mpn_montgomery_mulmod_n(T[1], Py, Qy, curve_p, curve_P, n, T[6]);
-    // t2 = Z1*Z2
-    mpn_montgomery_mulmod_n(T[2], Pz, Qz, curve_p, curve_P, n, T[6]);
-    // t3 = X1+Y1
-    mpn_montgomery_addmod_n(T[3], Px, Py, curve_p, n);
-    // t4 = X2+Y2
-    mpn_montgomery_addmod_n(T[4], Qx, Qy, curve_p, n);
-    // t3 = t3*t4
-    mpn_montgomery_mulmod_n(T[3], T[3], T[4], curve_p, curve_P, n, T[6]);
-    // t4 = t0+t1
-    mpn_montgomery_addmod_n(T[4], T[0], T[1], curve_p, n);
-    // t3 = t3-t4
-    mpn_montgomery_submod_n(T[3], T[3], T[4], curve_p, n);
-    // t4 = X1+Z1
-    mpn_montgomery_addmod_n(T[4], Px, Pz, curve_p, n);
-    // t5 = X2+Z2
-    mpn_montgomery_addmod_n(T[5], Qx, Qz, curve_p, n);
-    // t4 = t4*t5
-    mpn_montgomery_mulmod_n(T[4], T[4], T[5], curve_p, curve_P, n, T[6]);
-    // t5 = t0+t2
-    mpn_montgomery_addmod_n(T[5], T[0], T[2], curve_p, n);
-    // t4 = t4-t5
-    mpn_montgomery_submod_n(T[4], T[4], T[5], curve_p, n);
-    // t5 = Y1+Z1
-    mpn_montgomery_addmod_n(T[5], Py, Pz, curve_p, n);
-    // X3 = Y2+Z2
-    mpn_montgomery_addmod_n(Rx, Qy, Qz, curve_p, n);
-    // t5 = t5*X3
-    mpn_montgomery_mulmod_n(T[5], T[5], Rx, curve_p, curve_P, n, T[6]);
-    // X3 = t1+t2
-    mpn_montgomery_addmod_n(Rx, T[1], T[2], curve_p, n);
-    // t5 = t5-X3
-    mpn_montgomery_submod_n(T[5], T[5], Rx, curve_p, n);
-    // Z3 = a*t4
-    mpn_montgomery_mulmod_n(Rz, curve_aR, T[4], curve_p, curve_P, n, T[6]);
-    // X3 = b3*t2
-    mpn_montgomery_mulmod_n(Rx, curve_b3R, T[2], curve_p, curve_P, n, T[6]);
-    // Z3 = X3+Z3
-    mpn_montgomery_addmod_n(Rz, Rx, Rz, curve_p, n);
-    // X3 = t1-Z3
-    mpn_montgomery_submod_n(Rx, T[1], Rz, curve_p, n);
-    // Z3 = t1+Z3
-    mpn_montgomery_addmod_n(Rz, T[1], Rz, curve_p, n);
-    // Y3 = X3*Z3
-    mpn_montgomery_mulmod_n(Ry, Rx, Rz, curve_p, curve_P, n, T[6]);
-    // t1 = t0+t0
-    mpn_montgomery_lshift1mod_n(T[1], T[0], curve_p, n);
-    // t1 = t1+t0
-    mpn_montgomery_addmod_n(T[1], T[1], T[0], curve_p, n);
-    // t2 = a*t2
-    mpn_montgomery_mulmod_n(T[2], curve_aR, T[2], curve_p, curve_P, n, T[6]);
-    // t4 = b3*t4
-    mpn_montgomery_mulmod_n(T[4], curve_b3R, T[4], curve_p, curve_P, n, T[6]);
-    // t1 = t1+t2
-    mpn_montgomery_addmod_n(T[1], T[1], T[2], curve_p, n);
-    // t2 = t0-t2
-    mpn_montgomery_submod_n(T[2], T[0], T[2], curve_p, n);
-    // t2 = a*t2
-    mpn_montgomery_mulmod_n(T[2], curve_aR, T[2], curve_p, curve_P, n, T[6]);
-    // t4 = t4+t2
-    mpn_montgomery_addmod_n(T[4], T[4], T[2], curve_p, n);
-    // t0 = t1*t4
-    mpn_montgomery_mulmod_n(T[0], T[1], T[4], curve_p, curve_P, n, T[6]);
-    // Y3 = Y3+t0
-    mpn_montgomery_addmod_n(Ry, Ry, T[0], curve_p, n);
-    // t0 = t5*t4
-    mpn_montgomery_mulmod_n(T[0], T[5], T[4], curve_p, curve_P, n, T[6]);
-    // X3 = t3*X3
-    mpn_montgomery_mulmod_n(Rx, T[3], Rx, curve_p, curve_P, n, T[6]);
-    // X3 = X3-t0
-    mpn_montgomery_submod_n(Rx, Rx, T[0], curve_p, n);
-    // t0 = t3*t1
-    mpn_montgomery_mulmod_n(T[0], T[3], T[1], curve_p, curve_P, n, T[6]);
-    // Z3 = t5*Z3
-    mpn_montgomery_mulmod_n(Rz, T[5], Rz, curve_p, curve_P, n, T[6]);
-    // Z3 = Z3+t0
-    mpn_montgomery_addmod_n(Rz, Rz, T[0], curve_p, n);
+    // u = Y2*Z1
+    mpn_montgomery_mulmod_n(T[0], Qy, Pz, curve_p, curve_P, n, T[11]);
+    // u = u-Y1
+    mpn_montgomery_submod_n(T[0], T[0], Py, curve_p, n);
+    // uu = u**2
+    mpn_montgomery_sqrmod_n(T[1], T[0], curve_p, curve_P, n, T[11]);
+    // v = X2*Z1
+    mpn_montgomery_mulmod_n(T[2], Qx, Pz, curve_p, curve_P, n, T[11]);
+    // v = v-X1
+    mpn_montgomery_submod_n(T[2], T[2], Px, curve_p, n);
+    // vv = v**2
+    mpn_montgomery_sqrmod_n(T[3], T[2], curve_p, curve_P, n, T[11]);
+    // vvv = v*vv
+    mpn_montgomery_mulmod_n(T[4], T[2], T[3], curve_p, curve_P, n, T[11]);
+    // R = vv*X1
+    mpn_montgomery_mulmod_n(T[5], T[3], Px, curve_p, curve_P, n, T[11]);
+    // A = uu*Z1
+    mpn_montgomery_mulmod_n(T[6], T[1], Pz, curve_p, curve_P, n, T[11]);
+    // A = A-vvv
+    mpn_montgomery_submod_n(T[6], T[6], T[4], curve_p, n);
+    // A = A-R
+    mpn_montgomery_submod_n(T[6], T[6], T[5], curve_p, n);
+    // A = A-R
+    mpn_montgomery_submod_n(T[6], T[6], T[5], curve_p, n);
+    // X3 = v*A
+    mpn_montgomery_mulmod_n(Rx, T[2], T[6], curve_p, curve_P, n, T[11]);
+    // t5 = R-A
+    mpn_montgomery_submod_n(T[7], T[5], T[6], curve_p, n);
+    // t6 = vvv*Y1
+    mpn_montgomery_mulmod_n(T[8], T[4], Py, curve_p, curve_P, n, T[11]);
+    // Y3 = u*t5
+    mpn_montgomery_mulmod_n(Ry, T[0], T[7], curve_p, curve_P, n, T[11]);
+    // Y3 = Y3-t6
+    mpn_montgomery_submod_n(Ry, Ry, T[8], curve_p, n);
+    // Z3 = vvv*Z1
+    mpn_montgomery_mulmod_n(Rz, T[4], Pz, curve_p, curve_P, n, T[11]);
 }
 
 void ecc_pdbl(
@@ -135,68 +94,54 @@ void ecc_pdbl(
     ecc_ptemp T                                        // temporary variables, allocated with ecc_init_ptemp(T, n).
 )
 {
-    // t0 = X1**2
-    mpn_montgomery_sqrmod_n(T[0], Px, curve_p, curve_P, n, T[6]);
-    // t1 = Y1**2
-    mpn_montgomery_sqrmod_n(T[1], Py, curve_p, curve_P, n, T[6]);
-    // t2 = Z1**2
-    mpn_montgomery_sqrmod_n(T[2], Pz, curve_p, curve_P, n, T[6]);
-    // t3 = X1*Y1
-    mpn_montgomery_mulmod_n(T[3], Px, Py, curve_p, curve_P, n, T[6]);
-    // t3 = t3+t3
-    mpn_montgomery_lshift1mod_n(T[3], T[3], curve_p, n);
-    // Z3 = X1*Z1
-    mpn_montgomery_mulmod_n(Rz, Px, Pz, curve_p, curve_P, n, T[6]);
-    // Z3 = Z3+Z3
-    mpn_montgomery_lshift1mod_n(Rz, Rz, curve_p, n);
-    // X3 = a*Z3
-    mpn_montgomery_mulmod_n(Rx, curve_aR, Rz, curve_p, curve_P, n, T[6]);
-    // Y3 = b3*t2
-    mpn_montgomery_mulmod_n(Ry, curve_b3R, T[2], curve_p, curve_P, n, T[6]);
-    // Y3 = X3+Y3
-    mpn_montgomery_addmod_n(Ry, Rx, Ry, curve_p, n);
-    // X3 = t1-Y3
-    mpn_montgomery_submod_n(Rx, T[1], Ry, curve_p, n);
-    // Y3 = t1+Y3
-    mpn_montgomery_addmod_n(Ry, T[1], Ry, curve_p, n);
-    // Y3 = X3*Y3
-    mpn_montgomery_mulmod_n(Ry, Rx, Ry, curve_p, curve_P, n, T[6]);
-    // X3 = t3*X3
-    mpn_montgomery_mulmod_n(Rx, T[3], Rx, curve_p, curve_P, n, T[6]);
-    // Z3 = b3*Z3
-    mpn_montgomery_mulmod_n(Rz, curve_b3R, Rz, curve_p, curve_P, n, T[6]);
-    // t2 = a*t2
-    mpn_montgomery_mulmod_n(T[2], curve_aR, T[2], curve_p, curve_P, n, T[6]);
-    // t3 = t0-t2
-    mpn_montgomery_submod_n(T[3], T[0], T[2], curve_p, n);
-    // t3 = a*t3
-    mpn_montgomery_mulmod_n(T[3], curve_aR, T[3], curve_p, curve_P, n, T[6]);
-    // t3 = t3+Z3
-    mpn_montgomery_addmod_n(T[3], T[3], Rz, curve_p, n);
-    // Z3 = t0+t0
-    mpn_montgomery_lshift1mod_n(Rz, T[0], curve_p, n);
-    // t0 = Z3+t0
-    mpn_montgomery_addmod_n(T[0], Rz, T[0], curve_p, n);
-    // t0 = t0+t2
-    mpn_montgomery_addmod_n(T[0], T[0], T[2], curve_p, n);
-    // t0 = t0*t3
-    mpn_montgomery_mulmod_n(T[0], T[0], T[3], curve_p, curve_P, n, T[6]);
-    // Y3 = Y3+t0
-    mpn_montgomery_addmod_n(Ry, Ry, T[0], curve_p, n);
-    // t2 = Y1*Z1
-    mpn_montgomery_mulmod_n(T[2], Py, Pz, curve_p, curve_P, n, T[6]);
-    // t2 = t2+t2
-    mpn_montgomery_lshift1mod_n(T[2], T[2], curve_p, n);
-    // t0 = t2*t3
-    mpn_montgomery_mulmod_n(T[0], T[2], T[3], curve_p, curve_P, n, T[6]);
-    // X3 = X3-t0
-    mpn_montgomery_submod_n(Rx, Rx, T[0], curve_p, n);
-    // Z3 = t2*t1
-    mpn_montgomery_mulmod_n(Rz, T[2], T[1], curve_p, curve_P, n, T[6]);
-    // Z3 = Z3+Z3
-    mpn_montgomery_lshift1mod_n(Rz, Rz, curve_p, n);
-    // Z3 = Z3+Z3
-    mpn_montgomery_lshift1mod_n(Rz, Rz, curve_p, n);
+    // XX = X1**2
+    mpn_montgomery_sqrmod_n(T[0], Px, curve_p, curve_P, n, T[11]);
+    // ZZ = Z1**2
+    mpn_montgomery_sqrmod_n(T[1], Pz, curve_p, curve_P, n, T[11]);
+    // t0 = XX+XX
+    mpn_montgomery_lshift1mod_n(T[2], T[0], curve_p, n);
+    // t0 = t0+XX
+    mpn_montgomery_addmod_n(T[2], T[2], T[0], curve_p, n);
+    // w = a*ZZ
+    mpn_montgomery_mulmod_n(T[3], curve_aR, T[1], curve_p, curve_P, n, T[11]);
+    // w = w+t0
+    mpn_montgomery_addmod_n(T[3], T[3], T[2], curve_p, n);
+    // s = Y1*Z1
+    mpn_montgomery_mulmod_n(T[4], Py, Pz, curve_p, curve_P, n, T[11]);
+    // s = s+s
+    mpn_montgomery_lshift1mod_n(T[4], T[4], curve_p, n);
+    // ss = s**2
+    mpn_montgomery_sqrmod_n(T[5], T[4], curve_p, curve_P, n, T[11]);
+    // Z3 = s*ss
+    mpn_montgomery_mulmod_n(Rz, T[4], T[5], curve_p, curve_P, n, T[11]);
+    // R = Y1*s
+    mpn_montgomery_mulmod_n(T[6], Py, T[4], curve_p, curve_P, n, T[11]);
+    // RR = R**2
+    mpn_montgomery_sqrmod_n(T[7], T[6], curve_p, curve_P, n, T[11]);
+    // t3 = X1+R
+    mpn_montgomery_addmod_n(T[8], Px, T[6], curve_p, n);
+    // B = t3**2
+    mpn_montgomery_sqrmod_n(T[9], T[8], curve_p, curve_P, n, T[11]);
+    // B = B-XX
+    mpn_montgomery_submod_n(T[9], T[9], T[0], curve_p, n);
+    // B = B-RR
+    mpn_montgomery_submod_n(T[9], T[9], T[7], curve_p, n);
+    // h = w**2
+    mpn_montgomery_sqrmod_n(T[10], T[3], curve_p, curve_P, n, T[11]);
+    // h = h-B
+    mpn_montgomery_submod_n(T[10], T[10], T[9], curve_p, n);
+    // h = h-B
+    mpn_montgomery_submod_n(T[10], T[10], T[9], curve_p, n);
+    // X3 = h*s
+    mpn_montgomery_mulmod_n(Rx, T[10], T[4], curve_p, curve_P, n, T[11]);
+    // Y3 = B-h
+    mpn_montgomery_submod_n(Ry, T[9], T[10], curve_p, n);
+    // Y3 = w*Y3
+    mpn_montgomery_mulmod_n(Ry, T[3], Ry, curve_p, curve_P, n, T[11]);
+    // Y3 = Y3-RR
+    mpn_montgomery_submod_n(Ry, Ry, T[7], curve_p, n);
+    // Y3 = Y3-RR
+    mpn_montgomery_submod_n(Ry, Ry, T[7], curve_p, n);
 }
 
 void ecc_pmul(
--- src/ecc_proj.h
+++ src/ecc_proj.h
@@ -3,17 +3,15 @@
 
 #include <gmp.h>
 
-typedef mp_limb_t* ecc_ptemp[7];
+typedef mp_limb_t* ecc_ptemp[12];
 
 void ecc_init_ptemp(ecc_ptemp T, mp_size_t n);
 void ecc_free_ptemp(ecc_ptemp T);
-void ecc_padd(
+void ecc_padd_Qz_is_1(
     mp_limb_t* Rx, mp_limb_t* Ry, mp_limb_t* Rz,       // Rx, Ry, Rz must have n limbs allocated
     mp_limb_t* Px, mp_limb_t* Py, mp_limb_t* Pz,       // Px, Ry, Pz must have n limbs allocated
-    mp_limb_t* Qx, mp_limb_t* Qy, mp_limb_t* Qz,       // Qx, Qy, Qz must have n limbs allocated
+    mp_limb_t* Qx, mp_limb_t* Qy,                      // Qx, Qy     must have n limbs allocated
 
-    mp_limb_t* curve_aR,                               // curve_aR must have n limbs allocated
-    mp_limb_t* curve_b3R,                              // curve_b3R must have n limbs allocated
     mp_limb_t* curve_p,                                // curve_p must have n limbs allocated
     mp_limb_t* curve_P,                                // curve_P must have n limbs allocated
     mp_size_t n,                                       // number of limbs in curve->p
@@ -29,21 +27,6 @@ void ecc_pdbl(
     mp_limb_t* curve_b3R,                              // curve_b3R must have n limbs allocated
     mp_limb_t* curve_p,                                // curve_p must have n limbs allocated
     mp_limb_t* curve_P,                                // curve_P must have n limbs allocated
-    mp_size_t n,                                       // number of limbs in curve->p
-    
-    ecc_ptemp T                                        // temporary variables, allocated with ecc_init_ptemp(T, n).
-);
-
-void ecc_pmul(
-    mp_limb_t* Rx, mp_limb_t* Ry, mp_limb_t* Rz,       // Rx, Ry, Rz must have n limbs allocated
-    mp_limb_t* Px, mp_limb_t* Py, mp_limb_t* Pz,       // Px, Ry, Pz must have n limbs allocated
-
-    mp_limb_t* k,                                      // k must have n limbs allocated
-
-    mp_limb_t* curve_aR,                               // curve_aR must have n limbs allocated
-    mp_limb_t* curve_b3R,                              // curve_b3R must have n limbs allocated
-    mp_limb_t* curve_p,                                // curve_p must have n limbs allocated
-    mp_limb_t* curve_P,                                // curve_p must have n limbs allocated
     mp_size_t n,                                       // number of limbs in curve->p
     
     ecc_ptemp T                                        // temporary variables, allocated with ecc_init_ptemp(T, n).
