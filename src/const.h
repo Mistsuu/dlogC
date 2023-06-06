4a0f4e63d4de7983b7b3fb6619e6fe6555efe4f0
Mistsuu <vietanhtran12@gmail.com>
2023-06-06 23:00:26 +0700

    remove consts

--- src/const.h
+++ src/const.h
@@ -7,8 +7,6 @@
 
 #define DLOG_SUCCESS             0
 #define DLOG_NOT_FOUND_DLOG      1
-#define DLOG_CANNOT_ALLOCATE     2 // todo: remove
-#define DLOG_INVALID_UPPER_K     3 // todo: remove
 #define DLOG_MOVE_TO_NEXT_STEP   4
 #define DLOG_POINT_NOT_ON_CURVE  5
 #define DLOG_FAULTY_POINT_ORDER  6
