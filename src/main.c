ed1e6f173f7aee330262649d5c38f668085fed37
Mistsuu <vietanhtran12@gmail.com>
2023-06-24 02:18:04 +0700

    main works

--- src/main.c
+++ src/main.c
@@ -1,92 +1,195 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <stdlib.h>
 #include <gmp.h>
 #include "ecc.h"
-#include "ecc_proj.h"
 #include "ex_mpn.h"
-#include "ex_mpz.h"
 #include "dlog.h"
+#include "const.h"
+#include "mem.h"
 
-#define eccpt_sage_printf(P)                      \
-do {                                              \
-    printf("E(");                                 \
-    mpz_out_str(stdout, 10, P->x); printf(", ");  \
-    mpz_out_str(stdout, 10, P->y); printf(", ");  \
-    mpz_out_str(stdout, 10, P->z); printf(")\n"); \
-} while (0)
-
-#define mpnpt_sage_printf(Px, Py, Pz)   \
-do {                                    \
-    printf("E(");                       \
-    mpn_printf(Px, n); printf(", ");    \
-    mpn_printf(Py, n); printf(", ");    \
-    mpn_printf(Pz, n); printf(")\n");   \
-} while (0)
-
-void test1()
+const unsigned int BLOCK_BUFFER_SIZE = 1024;
+int str_init_readline(char** pbuffer)
 {
+    char c = 's';
+    int buffer_size = BLOCK_BUFFER_SIZE;
+    int str_len = 0;
+    (*pbuffer) = (char*) malloc_exit_when_null(buffer_size);
+
+    ssize_t nbytes_read;
+    while (c != '\0') {
+        // Read character
+        nbytes_read = read(STDIN_FILENO, &c, 1);
+        if (!nbytes_read || c == '\n' || c == '\0')
+            c = '\0';
+        else if (c < '0' || c > '9')
+            continue;
+
+        // Reallocate if needed
+        if (str_len == buffer_size) {
+            buffer_size += BLOCK_BUFFER_SIZE;
+            (*pbuffer) = (char*) realloc(*pbuffer, buffer_size);
+        }
+
+        (*pbuffer)[str_len++] = c;
+    }
+
+    return str_len;
+}
+
+unsigned int parse_arg_uint(char* arg, unsigned int default_val)
+{
+    long int val = default_val;
+    if (arg) {
+        val = strtol(arg, NULL, 10);
+        if (val == 0 || val == LONG_MAX || val == LONG_MIN)
+            val = default_val;
+        else
+            val = abs(val);
+    }
+    return (unsigned int) val;
+}
+
+size_t parse_arg_memval(char* arg, size_t default_memval)
+{
+    size_t mem_val = DEFAULT_MEM_LIMIT;
+    if (arg) 
+    {
+        char* unit = NULL;
+        double MEM_LIMIT_PER_UNIT = strtod(arg, &unit);
+
+        if (MEM_LIMIT_PER_UNIT == 0) {
+            mem_val = DEFAULT_MEM_LIMIT;
+        }
+        else if ((*unit) == 'M') {
+            if (SIZE_MAX / (1024 * 1024) < MEM_LIMIT_PER_UNIT)
+                mem_val = DEFAULT_MEM_LIMIT;
+            else
+                mem_val = (size_t) (MEM_LIMIT_PER_UNIT * 1024 * 1024);
+        }
+        else if ((*unit) == 'G' || (*unit) == 0) {
+            if (SIZE_MAX / (1024 * 1024 * 1024) < MEM_LIMIT_PER_UNIT)
+                mem_val = DEFAULT_MEM_LIMIT;
+            else
+                mem_val = (size_t) (MEM_LIMIT_PER_UNIT * 1024 * 1024) * 1024;
+        }
+        else {
+            mem_val = DEFAULT_MEM_LIMIT;
+        }
+    }
+    return mem_val;
+}
+
+void main(int argc, char** argv)
+{
+    // Get value from arguments.
+    unsigned int NUM_THREADS     = DEFAULT_NUM_THREADS;
+    unsigned int NUM_RAND_ITEMS  = DEFAULT_NUM_RAND_ITEMS;
+    size_t       MEM_LIMIT       = DEFAULT_MEM_LIMIT;
+    
+    int opt;
+    while ((opt = getopt(argc, argv, "t:m:r:h")) != -1) {
+        switch (opt) {
+            case 't':
+                NUM_THREADS = parse_arg_uint(optarg, DEFAULT_NUM_THREADS);
+                break;
+            case 'm':
+                MEM_LIMIT = parse_arg_memval(optarg, DEFAULT_MEM_LIMIT);
+                break;
+            case 'r':
+                NUM_RAND_ITEMS = parse_arg_uint(optarg, DEFAULT_NUM_RAND_ITEMS);
+                break;
+            default:
+                fprintf(stderr, "[usage]: %s [-t num_threads=4] [-m mem_limit=1G] [-r nrandpoints=20]\n", argv[0]);
+                exit(-1);
+        }
+    }
+
+    // Parse curve values from stdin.
+    char* curve_a;
+    char* curve_b;
+    char* curve_p;
+    char* Gx;
+    char* Gy;
+    char* kGx;
+    char* kGy;
+    char* n_str;
+
+    str_init_readline(&curve_a);
+    str_init_readline(&curve_b);
+    str_init_readline(&curve_p);
+    str_init_readline(&Gx);
+    str_init_readline(&Gy);
+    str_init_readline(&kGx);
+    str_init_readline(&kGy);
+    str_init_readline(&n_str);
+
+    // parse curve.
     ecc curve;
     ecc_init(
         curve, 
-        "435886898129897736",    // a 
-        "255399037257419668",    // b
-        "493424529467329699"     // p
+        curve_a, // a 
+        curve_b, // b
+        curve_p  // p
     );
 
-    printf("***************** SAGEMATH DEBUG CODE *****************\n");
-    printf("a = "); mpz_out_str(stdout, 10, curve->a); printf("\n");
-    printf("b = "); mpz_out_str(stdout, 10, curve->b); printf("\n");
-    printf("p = "); mpz_out_str(stdout, 10, curve->p); printf("\n");
-    printf("E = EllipticCurve(GF(p), [a, b])\n");
-
-    mpz_t n;
-    mpz_init_set_str(n, "493424528226388063", 10);
-    printf("n = ");
-    mpz_out_str(stdout, 10, n);
-    printf("\n");
-
+    // G
     eccpt G;
-    ecc_init_pt(G);
-    ecc_random_pt(curve, G);
-    printf("G = ");
-    eccpt_sage_printf(G);
+    ecc_init_pt_str(
+        curve, G,
+        Gx,   // x
+        Gy,   // y
+        NULL  // z
+    );
+    
+    // k*G
+    eccpt kG;
+    ecc_init_pt_str(
+        curve, kG,
+        kGx,
+        kGy,
+        NULL
+    );
 
+    // order of G.
+    mpz_t n;
+    mpz_init_set_str(n, n_str, 10);
+
+    // dlog() start.
     mpz_t k;
     mpz_init(k);
-    mpz_dev_urandomm(k, n);
+    if (dlog(
+            curve, 
+            k, 
+            G, kG, 
+            n, 
+            NUM_THREADS, 
+            MEM_LIMIT, 
+            NUM_RAND_ITEMS
+        ) == DLOG_SUCCESS
+    ) 
+    {
+        mpz_out_str(stdout, 10, k);
+        printf("\n");
+    }
+    else
+        printf("None\n");
 
-    eccpt kG;
-    ecc_init_pt(kG);
-    ecc_mul_noverify(curve, kG, G, k);
-    printf("kG = ");
-    eccpt_sage_printf(kG);
-
-    printf("EE = pari.ellinit([E.a4(), E.a6()], p)\n");
-    printf("pari.elllog(EE, kG, G, n)\n");
-
-    printf("*******************************************************\n\n");
-
-    dlog(
-        curve, 
-        k,
-        G, kG, 
-        n,
-        4,
-        1024 * 1024 * 1024,
-        20
-    );
-
-    printf("k = ");
-    mpz_out_str(stdout, 10, k);
-    printf("\n");
-
+    // cleanup.
     mpz_clear(k);
     mpz_clear(n);
+
     ecc_free_pt(G);
     ecc_free_pt(kG);
     ecc_free(curve);
-}
 
-int main()
-{
-    test1();
+    free(curve_a);
+    free(curve_b);
+    free(curve_p);
+    free(Gx);
+    free(Gy);
+    free(kGx);
+    free(kGy);
+    free(n_str);
 }
