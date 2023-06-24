18078e39f754ed3f676f45ad0c8f668cce6cc4c7
Mistsuu <vietanhtran12@gmail.com>
2023-06-24 16:28:42 +0700

    please use index 0.

--- src/dlog.c
+++ src/dlog.c
@@ -605,7 +605,7 @@ void* __thread__dlog_thread(
 
         // ---------- convert hare's item into an index of a hash table  -----------
         //              (if the hare item is a distinguished item)
-        if (hare_X_item[item_size_limbs - 1] % n_distmod == 0) {
+        if (hare_X_item[0] % n_distmod == 0) {
             size_t hash_index = (
                 (size_t) XXH3_64bits(hare_X_item, item_size_limbs * sizeof(mp_limb_t)) % n_hash_items
             );
