0c519def63f368087dca302d46f1d8f712cd43dc
Mistsuu <vietanhtran12@gmail.com>
2023-09-06 19:28:03 +0700

    Change sqrt(2/pi) to a constant

--- src/const.h
+++ src/const.h
@@ -1,23 +1,26 @@
 #ifndef CONST_H
 #define CONST_H
 
-#define AUTHOR                   "mistsuu"
-#define BASESIZE_PRIME_CHECKER   40
-#define UNDEFINED_COORDINATE     -1
+#define AUTHOR                      "mistsuu"
+#define BASESIZE_PRIME_CHECKER      40
+#define UNDEFINED_COORDINATE        -1
 
-#define DLOG_SUCCESS             0
-#define DLOG_NOT_FOUND_DLOG      1
-#define DLOG_P_IS_NOT_PRIME      2
-#define DLOG_MOVE_TO_NEXT_STEP   4
-#define DLOG_POINT_NOT_ON_CURVE  5
-#define DLOG_FAULTY_POINT_ORDER  6
-#define DLOG_BAD_CONFIG          7
-#define DLOG_BAD_COLLISION       8
+#define DLOG_SUCCESS                0
+#define DLOG_NOT_FOUND_DLOG         1
+#define DLOG_P_IS_NOT_PRIME         2
+#define DLOG_MOVE_TO_NEXT_STEP      4
+#define DLOG_POINT_NOT_ON_CURVE     5
+#define DLOG_FAULTY_POINT_ORDER     6
+#define DLOG_BAD_CONFIG             7
+#define DLOG_BAD_COLLISION          8
 
-#define DEFAULT_NUM_THREADS      4
-#define DEFAULT_NUM_CACHE_ITEMS  4
-#define DEFAULT_NUM_RAND_ITEMS   20
+#define DEFAULT_NUM_THREADS         4
+#define DEFAULT_NUM_CACHE_ITEMS     4
+#define DEFAULT_NUM_RAND_ITEMS      20
 
-#define SHARED_LIB_NAME          "libdlogfp"
+#define SHARED_LIB_NAME             "libdlogfp"
+
+#define SQRT_2_DIV_PI_NUMERATOR     123638
+#define SQRT_2_DIV_PI_DENOMINATOR   1549956
 
 #endif
